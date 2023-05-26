e7a054bfd2add58fa46bfb5f17f1701fbad488de
SighGPT <sigh@sighhpt.com>
2023-05-26 15:19:28 +0200

    läuft gerade -> jetzt html seiten anpassen

--- src/main/java/de/brightslearning/blog/controller/BlogController.java
+++ src/main/java/de/brightslearning/blog/controller/BlogController.java
@@ -11,10 +11,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class BlogController {
 
     //public BlogService bs;
-    
+
     @GetMapping("/")
     public String showHomePage(Model model){
        return "/home";
+    }
+
+    @GetMapping("/login")
+    public String showLogIn(Model model){
+        return "/login";
+    }
+
+    @GetMapping("/blog")
+    public String showBlog(Model model){
+        return "/fullEntry";
+    }
+
+    @GetMapping("/register")
+    public String showRegisterPage(Model model){
+        return "/register";
     }
 
 //    @PostMapping("/createEntry")
